73e2f6a407afbdb176fa639de54c24dfa851197f
Victor Okechukwu <vicoke360@gmail.com>
2023-06-10 17:09:06 +0100

    ----

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Footer from './components/Fotter'
-import SignUp from './components/SignUp';
+import SignUp from './components/Signup Page/SignUp';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
