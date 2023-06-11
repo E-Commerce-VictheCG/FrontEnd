b32626bcbc6d5ef8385ac75931190170d307337a
Victor Okechukwu <vicoke360@gmail.com>
2023-06-11 08:07:49 +0100

    Complete Signup flow

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Fotter'
 import SignUp from './components/Signup Page/SignUp';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import PrivateComponent from './components/private_component';
 
 function App() {
   return (
@@ -10,13 +11,15 @@ function App() {
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route path='/' element={<h1>Product Componet</h1>} />
-          <Route path='/add' element={<h1>Add Product Componet</h1>} />
-          <Route path='/update' element={<h1>Update Product Componet</h1>} />
-          <Route path='/logout' element={<h1>Logout Componet</h1>} />
-          <Route path='/profile' element={<h1>Profile Componet</h1>} />
-          <Route path='/signup' element={<SignUp />} />
-          {/* <Route path='/' element={<h1>Product Componet</h1>} /> */}
+          <Route element={<PrivateComponent />}>
+            <Route path='/' element={<h1>Product Componet</h1>} />
+            <Route path='/add' element={<h1>Add Product Componet</h1>} />
+            <Route path='/update' element={<h1>Update Product Componet</h1>} />
+            <Route path='/logout' element={<h1>Logout Componet</h1>} />
+            <Route path='/profile' element={<h1>Profile Componet</h1>} />
+            <Route path='/signup' element={<SignUp />} />
+            {/* <Route path='/' element={<h1>Product Componet</h1>} /> */}
+          </Route>
         </Routes>
         <Footer />
       </div>
