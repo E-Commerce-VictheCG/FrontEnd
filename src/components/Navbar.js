58fd6af2d55264d32526fbf8a1b04e90f9ab4b91
Victor Okechukwu <vicoke360@gmail.com>
2023-06-20 19:49:30 +0100

    Logout functionality integrated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 const Navbar = () => {
     const auth = localStorage.getItem('user')
-
+    // const navigate = useNavigate()
+    const logout = () => {
+        localStorage.clear();
+        // navigate('/signup')
+     }
     return (
         <nav className='navbar'>
             <ul className='navbar_ul'>
@@ -10,7 +14,7 @@ const Navbar = () => {
                 <li><Link to='/update'><p>Update Products</p></Link></li>
                 {/* <li><Link to='/logout'><p>logout</p></Link></li> */}
                 <li><Link to='/profile'><p>Profile</p></Link></li>
-                <li>{auth ? <Link to='/logout'><p>Logout</p></Link>:<Link to='/signup'><p>SignUp</p></Link>}</li>
+                <li>{auth ? <Link to='/signup' onClick={logout}><p>Logout</p></Link>:<Link to='/signup'><p>SignUp</p></Link>}</li>
             </ul>
         </nav>
     );
