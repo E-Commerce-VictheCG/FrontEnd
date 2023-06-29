b6c25f96b28e2acf840c2018a8cfc8da1710b901
Victor Okechukwu <vicoke360@gmail.com>
2023-06-30 00:30:16 +0100

    ProductList UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Fotter'
 import SignUp from './components/Signup Page/SignUp';
 import Login from './components/Login Page/Login';
-import Product from "./components/Product Page/product"
+import Product from "./components/Product Page/productList"
 import AddProduct from "./components/Add_Products/addProduct"
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import PrivateComponent from './components/private_component';
